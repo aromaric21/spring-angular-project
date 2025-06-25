92954875f13afd7a9fdda64ab0d87df005da379b
aromaric <aromaricadjei213@gmail.com>
2025-06-25 05:44:35 +0000

    First commit

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
+import { AdminTemplate } from './admin-template/admin-template';
+import {MatToolbar, MatToolbarModule} from '@angular/material/toolbar';
+import {MatButton, MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatMenu, MatMenuItem, MatMenuTrigger} from '@angular/material/menu';
 
 @NgModule({
   declarations: [
-    App
+    App,
+    AdminTemplate
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenu,
+    MatMenuTrigger,
+    MatMenuItem
   ],
   providers: [
     provideBrowserGlobalErrorListeners()
