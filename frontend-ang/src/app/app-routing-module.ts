38fd6ec75c17bec87e783bc7c4429f9c58d9ed0d
aromaric <aromaricadjei213@gmail.com>
2025-07-06 09:20:28 +0000

    feat:front-end part

---
+++ frontend-ang/src/app/app-routing-module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {Home} from './home/home';
+import {Profile} from './profile/profile';
+import {Login} from './login/login';
+import {LoadStudents} from './load-students/load-students';
+import {LoadPayments} from './load-payments/load-payments';
+import {Dashboard} from './dashboard/dashboard';
+import {Students} from './students/students';
+import {Payments} from './payments/payments';
+import {AdminTemplate} from './admin-template/admin-template';
+
+const routes: Routes = [
+  {path: "", component: Login},
+  {path: "login", component: Login},
+  {path: "admin", component: AdminTemplate, children: [
+      {path: "home", component: Home},
+      {path: "profile", component: Profile},
+      {path: "loadStudents", component: LoadStudents},
+      {path: "loadPayments", component: LoadPayments},
+      {path: "dashboard", component: Dashboard},
+      {path: "students", component: Students},
+      {path: "payments", component: Payments},
+    ]},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
